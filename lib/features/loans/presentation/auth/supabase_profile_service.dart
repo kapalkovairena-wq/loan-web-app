57a93f83ca6d8be2824546c1c8798b2a415798ca
kapalkovairena-wq <kapalkovairena@gmail.com>
2026-01-20 15:09:51 +0100

    Update

--- lib/features/loans/presentation/auth/supabase_profile_service.dart
+++ lib/features/loans/presentation/auth/supabase_profile_service.dart
@@ -5,14 +5,13 @@ import 'package:firebase_auth/firebase_auth.dart';
 class SupabaseProfileService {
   static Future<void> createProfile(User user) async {
     final url = Uri.parse(
-      "https://YOUR_PROJECT_ID.supabase.co/functions/v1/create-profile",
+      "https://yztryuurtkxoygpcmlmu.supabase.co/functions/v1/create-profile",
     );
 
     await http.post(
       url,
       headers: {
         "Content-Type": "application/json",
-        "X_APP_SECRET": "TON_SECRET",
       },
       body: jsonEncode({
         "firebase_uid": user.uid,
