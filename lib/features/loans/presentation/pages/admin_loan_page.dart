6dc79ea689c501140853c7f2d4bcc870624abe80
kapalkovairena-wq <kapalkovairena@gmail.com>
2026-01-23 15:22:47 +0100

    Update

--- lib/features/loans/presentation/pages/admin_loan_page.dart
+++ lib/features/loans/presentation/pages/admin_loan_page.dart
@@ -166,8 +166,8 @@ class _AdminLoanPageState extends State<AdminLoanPage> {
         Uri.parse('https://yztryuurtkxoygpcmlmu.supabase.co/functions/v1/send_contract_pdf'),
         headers: {
           'Content-Type': 'application/json',
-          "apikey": "VOTRE_API_KEY",
-          "Authorization": "Bearer VOTRE_API_KEY",
+          "apikey": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl6dHJ5dXVydGt4b3lncGNtbG11Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg3OTM0OTAsImV4cCI6MjA4NDM2OTQ5MH0.wJB7hDwviguUl_p3W4XYMdGGWv-mbi2yR6vTub432ls",
+          "Authorization": "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl6dHJ5dXVydGt4b3lncGNtbG11Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg3OTM0OTAsImV4cCI6MjA4NDM2OTQ5MH0.wJB7hDwviguUl_p3W4XYMdGGWv-mbi2yR6vTub432ls",
         },
         body: jsonEncode({
           "clientName": loanData['full_name'],
